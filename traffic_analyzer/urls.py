1f0d1ba2c52ac42d6ecb14a1c0403efa95ef13f1
lanzy-lanzy <gerlan.masterpay@gmail.com>
2024-11-25 07:23:56 +0800

    first commit

---
+++ traffic_analyzer/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('upload/', views.video_upload, name='video_upload'),
+    path('live/', views.live_detection, name='live_detection'),
+    path('analysis/<int:analysis_id>/results/', views.analysis_results, name='analysis_results'),
+    path('analysis/<int:analysis_id>/processing/', views.processing, name='processing'),
+    path('analysis/<int:analysis_id>/status/', views.analysis_status, name='analysis_status'),
+
+]
